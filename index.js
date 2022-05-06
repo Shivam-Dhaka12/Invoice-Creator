4f004de7a9e3126deb1c81ae70d0bd1ae9f6865f
Shivam Dhaka <shivamdhaka1200@gmai.com>
2022-05-06 09:53:56 +0530

    basic js added 9:54 06-05-22

---
+++ index.js
@@ -0,0 +1,52 @@
+const washcarEL = document.getElementById("washcar");
+const mowlawnEL = document.getElementById("mowlawn");
+const pullweedsEL = document.getElementById("pullweeds");
+const listEl = document.getElementById("list");
+const notesEl = document.getElementById("notes");
+const totalEl = document.getElementById("total");
+const sendbtnEl = document.getElementById("send-btn");
+
+let services = []
+let sum = 0;
+
+sendbtnEl.addEventListener("click" , function () {
+    listEl.innerHTML = "";
+    sum = 0;
+    notesEl.textContent = "";
+    totalEl.textContent = `$${sum}`;
+})
+
+function onClick(idEl) {
+    idEl.addEventListener("click",function () {
+        idEl.style.border = "2px solid #4A4E74";
+        idEl.disabled = true;
+        services.push(idEl.value);
+        localStorage.setItem("services",JSON.stringify(services));
+        renderitems(services);
+        if (!notesEl.textContent) {
+            notesEl.textContent = "We accept cash, credit card, or PayPal";
+        }
+        sum += 10;
+        totalEl.textContent = `$${sum}`;
+    })
+}
+
+onClick(washcarEL);
+onClick(mowlawnEL);
+onClick(pullweedsEL);
+
+function renderitems(services) {
+
+        let item = document.createElement("div");
+        item.className = "list-item";
+        let task  = document.createElement("div");
+        let price = document.createElement("div");
+        task.className = "task";
+        price.className = "price";
+        task.textContent = services[services.length -1];
+        price.textContent = "$10";
+        item.appendChild(task);
+        item.appendChild(price);
+        listEl.appendChild(item);
+
+}
